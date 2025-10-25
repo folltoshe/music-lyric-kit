9e91d4787a2765b7170e68dbc441cb49aef63e86
Folltoshe <folltoshe@foxmail.com>
2025-10-25 23:27:02 +0800

    chore: add dev script

--- packages/test/vite.config.ts
+++ packages/test/vite.config.ts
@@ -1,3 +1,17 @@
+import { join } from 'node:path'
+
 import { generateConfig } from '../../vite.config'
 
-export default generateConfig({})
+const src = join(process.cwd(), 'src')
+
+export default generateConfig({
+  root: src,
+  build: {
+    minify: false,
+    reportCompressedSize: false,
+  },
+  server: {
+    port: 9090,
+    strictPort: false,
+  },
+})
