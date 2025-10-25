bed8d2316feefd2175840313efbea0e4d8c507c3
Folltoshe <folltoshe@foxmail.com>
2025-10-26 03:11:12 +0800

    feat(core): add lrc plugin

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,17 @@
-export {}
+import type { LrcOptions, LrcParseProps } from '@music-lyric-kit/plugin-lrc'
+import { LrcParser } from '@music-lyric-kit/plugin-lrc'
+
+export type { LrcOptions, LrcParseProps }
+export { LrcParser }
+
+export interface LyricParserOptions {
+  lrc?: LrcOptions
+}
+
+export class LyricParser {
+  public lrc: LrcParser
+
+  constructor(options?: LyricParserOptions) {
+    this.lrc = new LrcParser(options?.lrc)
+  }
+}
