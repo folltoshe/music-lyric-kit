40ab7d867ae2e4bc8d0038f8bfe58c97ec8889a5
Folltoshe <folltoshe@foxmail.com>
2025-10-26 02:54:33 +0800

    feat(test): support parse lrc

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,1 +1,68 @@
-console.log('test')
+import type { LrcParseProps } from '@music-lyric-kit/plugin-lrc'
+
+import { LrcParser } from '@music-lyric-kit/plugin-lrc'
+
+const LOCAL_HISTORY_KEY = 'last-parse'
+
+const lrc = new LrcParser()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const parseBtn = document.getElementById('parse-btn')
+  const loadBtn = document.getElementById('load-btn')
+
+  const resultContainer = document.getElementById('result-container')
+  const resultElement = document.getElementById('result')
+
+  const originalElement = document.getElementById('original') as HTMLTextAreaElement
+  const dynamicElement = document.getElementById('dynamic') as HTMLTextAreaElement
+  const translateElement = document.getElementById('translate') as HTMLTextAreaElement
+  const romanElement = document.getElementById('roman') as HTMLTextAreaElement
+
+  if (!resultContainer || !resultElement) return
+
+  const handleParse = (content: LrcParseProps) => {
+    const result = lrc.parse(content)
+
+    console.log('Parse Result: ', result)
+
+    resultElement.textContent = JSON.stringify(result, null, 2)
+    resultContainer.style.display = 'block'
+
+    resultContainer.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  // 解析按钮点击事件
+  parseBtn?.addEventListener('click', () => {
+    const original = originalElement.value.trim()
+    const dynamic = dynamicElement.value.trim() || ''
+    const translate = translateElement.value.trim() || ''
+    const roman = romanElement.value.trim() || ''
+
+    if (!original) return
+
+    const history = JSON.stringify({ original, dynamic, translate, roman })
+    localStorage.setItem(LOCAL_HISTORY_KEY, history)
+
+    handleParse({
+      original,
+      dynamic,
+      translate,
+      roman,
+    })
+  })
+
+  loadBtn?.addEventListener('click', () => {
+    const history = localStorage.getItem(LOCAL_HISTORY_KEY)
+    if (!history) return
+    try {
+      const parsed = JSON.parse(history)
+
+      originalElement.textContent = String(parsed.original).trim() || ''
+      dynamicElement.textContent = String(parsed.dynamic).trim() || ''
+      translateElement.textContent = String(parsed.translate).trim() || ''
+      romanElement.textContent = String(parsed.roman).trim() || ''
+
+      handleParse(parsed)
+    } catch {}
+  })
+})
