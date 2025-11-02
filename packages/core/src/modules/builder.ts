a3fbe546c6cb53b55eff2e4a0f4cb4046efbceca
Folltoshe <folltoshe@foxmail.com>
2025-11-02 20:51:12 +0800

    feat(core): support set common config
    Breaking Change: parser and builder init params have been changed

--- packages/core/src/modules/builder.ts
+++ packages/core/src/modules/builder.ts
@@ -1,13 +1,32 @@
+import type { DeepPartial } from '@music-lyric-kit/shared'
+
+import { ConfigManager, Builder } from '@music-lyric-kit/shared'
+
 import { LrcPlugin } from '@root/plugins'
 
 export interface LyricBuilderOptions {
-  lrc?: LrcPlugin.Builder.Config
+  common?: Builder.Config.Full
+  plugin?: {
+    lrc?: LrcPlugin.Builder.Config
+  }
 }
 
 export class LyricBuilder {
+  private common?: ConfigManager<Builder.Config.Full>
+
   public lrc: LrcPlugin.Builder.Plugin
 
   constructor(options?: LyricBuilderOptions) {
-    this.lrc = new LrcPlugin.Builder.Plugin(options?.lrc)
+    if (options?.common) {
+      this.common = new ConfigManager(Builder.Config.FULL, options.common)
+    }
+    this.lrc = new LrcPlugin.Builder.Plugin(options?.plugin?.lrc)
+  }
+
+  public updateCommonConfig(target: DeepPartial<Builder.Config.Full>) {
+    if (!this.common) {
+      return
+    }
+    this.common.set(target)
   }
 }
--- packages/core/src/modules/parser.ts
+++ packages/core/src/modules/parser.ts
@@ -1,17 +1,35 @@
-import { LrcPlugin } from '@root/plugins'
-import { TtmlPlugin } from '@root/plugins'
+import type { DeepPartial } from '@music-lyric-kit/shared'
+
+import { ConfigManager, Parser } from '@music-lyric-kit/shared'
+
+import { LrcPlugin, TtmlPlugin } from '@root/plugins'
 
 export interface LyricParserOptions {
-  lrc?: LrcPlugin.Parser.Config
-  ttml?: TtmlPlugin.Parser.Config
+  common?: Parser.Config.Full
+  plugin?: {
+    lrc?: LrcPlugin.Parser.Config
+    ttml?: TtmlPlugin.Parser.Config
+  }
 }
 
 export class LyricParser {
+  private common?: ConfigManager<Parser.Config.Full>
+
   public lrc: LrcPlugin.Parser.Plugin
   public ttml: TtmlPlugin.Parser.Plugin
 
   constructor(options?: LyricParserOptions) {
-    this.lrc = new LrcPlugin.Parser.Plugin(options?.lrc)
-    this.ttml = new TtmlPlugin.Parser.Plugin(options?.ttml)
+    if (options?.common) {
+      this.common = new ConfigManager(Parser.Config.FULL, options.common)
+    }
+    this.lrc = new LrcPlugin.Parser.Plugin(options?.plugin?.lrc, this.common)
+    this.ttml = new TtmlPlugin.Parser.Plugin(options?.plugin?.ttml, this.common)
+  }
+
+  public updateCommonConfig(target: DeepPartial<Parser.Config.Full>) {
+    if (!this.common) {
+      return
+    }
+    this.common.set(target)
   }
 }
